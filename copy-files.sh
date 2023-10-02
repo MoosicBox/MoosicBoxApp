d0ebb6630e3cd6e21394439bf65c81d092d35c72
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-10-01 22:35:09 -0400

    Add copy-files.sh

---
+++ copy-files.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+dir=$1
+[[ -z $dir ]] && dir='../MoosicBoxUI'
+
+echo "Copying $dir/src/components"
+rm -rf src/components && cp -r "$dir/src/components" src/components
+echo "Copying $dir/src/routes"
+rm -rf src/routes && cp -r "$dir/src/routes" src/routes
+echo "Copying $dir/src/services"
+rm -rf src/services && cp -r "$dir/src/services" src/services
+echo "Copying $dir/public"
+rm -rf public && cp -r "$dir/public" public
+
