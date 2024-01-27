3858c05fb74846288e39ecf68b326f41546568f4
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-01-26 20:43:04 -0500

    Copy src/pages

--- copy-files.sh
+++ copy-files.sh
@@ -5,10 +5,11 @@ dir=$1
 
 echo "Copying $dir/src/components"
 rm -rf src/components && cp -r "$dir/src/components" src/components
+echo "Copying $dir/src/pages"
+rm -rf src/pages && cp -r "$dir/src/pages" src/pages
 echo "Copying $dir/src/routes"
 rm -rf src/routes && cp -r "$dir/src/routes" src/routes
 echo "Copying $dir/src/services"
 rm -rf src/services && cp -r "$dir/src/services" src/services
 echo "Copying $dir/public"
 rm -rf public && cp -r "$dir/public" public
-
