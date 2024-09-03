eef5152d98836f2a0122a9cda9c831cc83c61d78
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-03 07:30:00 -0400

    MoosicBox/MoosicBoxUI#2 Add app-pages and app-styles directory overrides

--- copy-files.sh
+++ copy-files.sh
@@ -3,21 +3,25 @@
 dir=$1
 [[ -z $dir ]] && dir='../MoosicBoxUI'
 
-echo "Copying $dir/src/*/"
-rm -rf src/*/
 echo "Copying $dir/public"
 rm -rf public && cp -r "$dir/public" public
 echo "Copying $dir/src/components"
+rm -r src/components
 cp -r "$dir/src/components" src/components
 echo "Copying $dir/src/layouts"
+rm -r src/layouts
 cp -r "$dir/src/layouts" src/layouts
 echo "Copying $dir/src/middleware"
+rm -r src/middleware
 cp -r "$dir/src/middleware" src/middleware
 echo "Copying $dir/src/pages"
+rm -r src/pages
 cp -r "$dir/src/pages" src/pages
 echo "Copying $dir/src/routes"
+rm -r src/routes
 cp -r "$dir/src/routes" src/routes
 echo "Copying $dir/src/services"
+rm -r src/services
 cp -r "$dir/src/services" src/services
 echo "Copying $dir/src/env.d.ts"
 cp "$dir/src/env.d.ts" src/env.d.ts
@@ -25,3 +29,9 @@ echo "Copying $dir/src/sst-env.d.ts"
 cp "$dir/src/sst-env.d.ts" src/sst-env.d.ts
 echo "Copying $dir/render-directive"
 cp -r "$dir/render-directive" ./
+echo "Copying $dir/app-pages"
+mkdir -p ./src/pages/
+cp -r src/app-pages/* ./src/pages/
+echo "Copying $dir/app-styles"
+mkdir -p ./src/styles/
+cp -r src/app-styles/* ./src/styles/
