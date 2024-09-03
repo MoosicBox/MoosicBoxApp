e82d9751b52a2cf215faee55d3237c7ba9b96b25
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-03 07:30:00 -0400

    MoosicBox/MoosicBoxUI#2 Add app-* directories

--- copy-files.sh
+++ copy-files.sh
@@ -4,34 +4,48 @@ dir=$1
 [[ -z $dir ]] && dir='../MoosicBoxUI'
 
 echo "Copying $dir/public"
-rm -rf public && cp -r "$dir/public" public
+rm -r public
+mkdir -p public
+cp -rT "$dir/public" public
+cp -rT app-public/. public
 echo "Copying $dir/src/components"
 rm -r src/components
-cp -r "$dir/src/components" src/components
+mkdir -p src/components
+cp -rT "$dir/src/components" src/components
+cp -rT src/app-components/. src/components
 echo "Copying $dir/src/layouts"
 rm -r src/layouts
-cp -r "$dir/src/layouts" src/layouts
+mkdir -p src/layouts
+cp -rT "$dir/src/layouts" src/layouts
+cp -rT src/app-layouts/. src/layouts
 echo "Copying $dir/src/middleware"
 rm -r src/middleware
-cp -r "$dir/src/middleware" src/middleware
+mkdir -p src/middleware
+cp -rT "$dir/src/middleware" src/middleware
+cp -rT src/app-middleware/. src/middleware
 echo "Copying $dir/src/pages"
 rm -r src/pages
-cp -r "$dir/src/pages" src/pages
+mkdir -p src/pages
+cp -rT "$dir/src/pages" src/pages
+cp -rT src/app-pages/. src/pages
 echo "Copying $dir/src/routes"
 rm -r src/routes
-cp -r "$dir/src/routes" src/routes
+mkdir -p src/routes
+cp -rT "$dir/src/routes" src/routes
+cp -rT src/app-routes/. src/routes
 echo "Copying $dir/src/services"
 rm -r src/services
-cp -r "$dir/src/services" src/services
+mkdir -p src/services
+cp -rT "$dir/src/services" src/services
+cp -rT src/app-services/. src/services
+echo "Copying $dir/src/styles"
+rm -r src/styles
+mkdir -p src/styles
+cp -rT "$dir/src/styles" src/styles
+cp -rT src/app-styles/. src/styles
 echo "Copying $dir/src/env.d.ts"
 cp "$dir/src/env.d.ts" src/env.d.ts
 echo "Copying $dir/src/sst-env.d.ts"
 cp "$dir/src/sst-env.d.ts" src/sst-env.d.ts
 echo "Copying $dir/render-directive"
 cp -r "$dir/render-directive" ./
-echo "Copying $dir/app-pages"
-mkdir -p ./src/pages/
-cp -r src/app-pages/* ./src/pages/
-echo "Copying $dir/app-styles"
-mkdir -p ./src/styles/
-cp -r src/app-styles/* ./src/styles/
