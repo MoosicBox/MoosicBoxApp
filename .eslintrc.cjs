820ce7597a4449f3ceb767d6b1c03a89d96144b6
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-07 13:39:00 -0400

    MoosicBox/MoosicBoxUI#2 Handle astro files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -41,6 +41,7 @@ module.exports = {
                 caughtErrorsIgnorePattern: '^_',
             },
         ],
+        '@typescript-eslint/no-namespace': 'off',
     },
     overrides: [
         {
@@ -57,9 +58,26 @@ module.exports = {
             },
         },
         {
-            files: ['**/sst-env.d.ts'],
+            files: ['**/sst-env.d.ts', 'sst.config.ts', '**/infra/*.ts'],
             rules: {
                 '@typescript-eslint/triple-slash-reference': 'off',
+            },
+        },
+        {
+            // Define the configuration for `.astro` file.
+            files: ['*.astro'],
+            // Allows Astro components to be parsed.
+            parser: 'astro-eslint-parser',
+            // Parse the script in `.astro` as TypeScript by adding the following configuration.
+            // It's the setting you need when using TypeScript.
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                extraFileExtensions: ['.astro'],
+            },
+            rules: {
+                // override/add rules settings here, such as:
+                // "astro/no-set-html-directive": "error"
+                '@typescript-eslint/naming-convention': 'off',
             },
         },
     ],
